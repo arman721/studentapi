d0e9b144dabb87b7d5c6d3a40444c9938011d896
Arman <119151658+arman721@users.noreply.github.com>
2024-10-14 09:55:08 +0530

    implemented update and delete of student details

--- studentapi/views.py
+++ studentapi/views.py
@@ -41,3 +41,33 @@ def Studentapi(request):
             return HttpResponse(json_data)
         json_data=JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data)
+    if request.method=="PUT":
+        data=request.body
+        stream=io.BytesIO(data)
+        python_data=JSONParser().parse(stream)
+        id=python_data["id"]
+        stu=Students.objects.get(id=id)
+        serializer=StudentSerializer(stu,data=python_data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            data={"msg":"data updated"}
+            json_data=JSONRenderer().render(data)
+            return HttpResponse(json_data)
+        json_data=JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data)
+    if request.method=="DELETE":
+        data=request.body
+        stream=io.BytesIO(data)
+        python_data=JSONParser().parse(stream)
+        ids=python_data["id"]
+        for id in ids:
+            try:
+                Students.objects.get(id=id).delete()
+            except Students.DoesNotExist:
+                res = {"msg": f"Student with ID {id} does not exist."}
+                json_data = JSONRenderer().render(res)
+                return HttpResponse(json_data, content_type="application/json", status=404)
+       
+        res={"msg":"data deleted"}
+        json_data=JSONRenderer().render(res)
+        return HttpResponse(json_data)
