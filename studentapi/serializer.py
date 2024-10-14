548904518d680d694f4bbe0d8e3ab9744923583c
Arman <119151658+arman721@users.noreply.github.com>
2024-10-14 09:50:05 +0530

    Update serializer.py

--- studentapi/serializer.py
+++ studentapi/serializer.py
@@ -10,11 +10,3 @@ class StudentSerializer(serializers.Serializer):
     
     def create(self, validated_data):
         return Students.objects.create(**validated_data)
-    def update(self, instance, validated_data):
-
-        instance.name=validated_data.get('name',instance.name)
-
-        instance.age=validated_data.get('age',instance.age)
-        instance.city=validated_data.get('city',instance.city)
-        instance.save()
-        return instance
